7924105d1ce8d6681fe992177184360dd88e2b26
Alan Pecenkovic <alanquad@yahoo.com>
2021-11-24 23:54:15 +0100

    conditional rendering of elements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,6 +14,21 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === filterYear;
   });
 
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map(expense => {
+      return (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      );
+    });
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -21,36 +36,7 @@ function Expenses(props) {
           selected={filterYear}
           filterChange={handleFilterChange}
         />
-        {filteredExpenses.map(expense => {
-          return (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          );
-        })}
-        {/* <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        />
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        />
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        />
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        /> */}
+        {expensesContent}
       </Card>
     </div>
   );
